1e47b7a852e6c0dd946696cba5813d131777969e
Guilherme G. Silva <dev.gsilv@gmail.com>
2023-07-08 22:13:56 -0300

    feat: DB folder structure.

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 require('dotenv').config()
 const express = require('express')
-const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const app = express()
 app.use(express.json())
 
-// MONGO ATLAS CONNECTION
-const dbUser = process.env.DB_USER
-const dbPass = process.env.DB_PASS
+const apiPort = 3000
+app.listen(apiPort)
+console.log(`API running on port ${apiPort}`)
 
-mongoose
-    .connect(`mongodb+srv://${dbUser}:${dbPass}@teste0.oyxzwqn.mongodb.net/?retryWrites=true&w=majority`)
-    .then(() => {
-        app.listen(3000)
-        console.log('API running on port 3000')
-    })
-    .catch((e) => console.error(e))
+// DB CONNECTION
+const conn = require("./db/mongoDB");
+conn();
 
 // Routes
 const routes = require('./routes/router')
