d31a91b85d226eef4e1fa19908ddb019917af5c8
Guilherme G. Silva <dev.gsilv@gmail.com>
2023-07-13 17:08:08 -0300

    yact: yet another cicd test #13.

--- controllers/healthController.js
+++ controllers/healthController.js
@@ -1,6 +1,6 @@
 export const check = (req, res) => {
     try {
-        res.status(200).send('Health check: PASS! Github Actions online!')
+        res.status(200).send('Health check: PASS! Github Actions online! Now for sure!')
     } catch (e) {
         res.status(400).send(e)
     }
